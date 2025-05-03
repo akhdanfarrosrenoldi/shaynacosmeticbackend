6f55a7f5c4d8a9460fb149bd2b4d09f46282f67b
akhdanfarrosrenoldi <renoldiakhdanfarros@gmail.com>
2025-05-03 17:04:45 +0700

    api

--- app/Http/Controllers/Api/BookingTransactionController.php
+++ app/Http/Controllers/Api/BookingTransactionController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreBookingTransactionRequest;
 use Illuminate\Http\Request;
 
 class BookingTransactionController extends Controller
 {
     //
+    public function store(StoreBookingTransactionRequest $request)
+    {
+        try {
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'An error occured', 'error' => $e->getMessage()], 500);
+        }
+    }
 }
